d068a86446fd4d2d70524e341edba5ad24268a61
nicolasjrz <nicolasjuarez1234@gmail.com>
2024-11-15 13:00:18 -0300

    init proyect

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+	/* config options here */
+	images: {
+		domains: ["demos.creative-tim.com"], // Reemplaza con tus dominios
+	},
+};
+
+export default nextConfig;
