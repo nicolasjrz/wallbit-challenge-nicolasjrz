5e8101f3fafa3e78cc201dd2d586c3a8269bba59
nicolasjrz <nicolasjuarez1234@gmail.com>
2024-11-18 16:09:24 -0300

    add array roulette

---
+++ src/data/array.ts
@@ -0,0 +1,18 @@
+import perro from "../../public/images/perro1.jpg";
+import perro2 from "../../public/images/perro2.jpg";
+import perro3 from "../../public/images/perro3.jpg";
+
+export const arrayImagesRoulette = [
+	{
+		id: 1,
+		url: perro,
+	},
+	{
+		id: 2,
+		url: perro2,
+	},
+	{
+		id: 3,
+		url: perro3,
+	},
+];
