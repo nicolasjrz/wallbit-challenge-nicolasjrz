11ae4145dd4da0e34d6b4abc18fd869e68264892
nicolasjrz <nicolasjuarez1234@gmail.com>
2024-11-18 15:44:04 -0300

    config tailwind and next

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	// reactStrictMode: true,
+	// swcMinify: true,
+	// i18n: {
+	// 	defaultLocale: "en",
+	// 	locales: ["en", "hi"],
+	// },
 	images: {
-		domains: ["demos.creative-tim.com"], // Reemplaza con tus dominios
+		domains: ["demos.creative-tim.com", "fakestoreapi.com"], // Reemplaza con tus dominios
+
+		// path: "/",
 	},
 };
 
